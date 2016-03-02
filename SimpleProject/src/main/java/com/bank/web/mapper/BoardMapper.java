6288d22666b07037aef6d10bd61f739a88d78f1f
user <user@user-PC>
2016-03-02 17:24:09 +0900

    민호,연식 역할분담

--- SimpleProject/src/main/java/com/bank/web/mapper/BoardMapper.java
+++ SimpleProject/src/main/java/com/bank/web/mapper/BoardMapper.java
@@ -7,16 +7,16 @@ import com.bank.web.domain.BoardVO;
 @Repository
 public interface BoardMapper {
 
-	public BoardVO[] selectBoard();
+	public BoardVO[] selectBoard();	//연식
 
-	public BoardVO[] selectBoardByTitle(String title);
+	public BoardVO[] selectBoardByTitle(String title);	//검색
 
-	public int insertBoard(String id, String title, String context);
+	public int insertBoard(String id, String title, String context);	//연식
 
-	public int updateBoard(String title, String context);
+	public int updateBoard(String title, String context);	//민호
 
-	public int deleteMember(String index);
+	public int deleteMember(String index);	//민호
 
-	public String selectUserWritten(String index, String id);
+	public String selectUserWritten(String index, String id);	//민호
 
 }
