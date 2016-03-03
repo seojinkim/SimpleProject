6a453e60435d73417f5519ba5fa55a82ccffa5be
user <user@user-PC>
2016-03-03 11:09:43 +0900

    board!!!!!!!!!!!!!

--- SimpleProject/src/main/java/com/bank/web/controller/BoardController.java
+++ SimpleProject/src/main/java/com/bank/web/controller/BoardController.java
@@ -1,81 +1,116 @@
 package com.bank.web.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bank.web.domain.BoardVO;
+import com.bank.web.domain.MemberVO;
+import com.bank.web.serviceImpl.BoardServiceImpl;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
-	
-	/*//로그인 후 게시판 보기
-	@RequestMapping(value = "/board", method = RequestMethod.GET)
-	public String myAccount(Model model) {
 
-		BoardVO board = new BoardVO();
-		acc = boardService.
-		if (acc == null) {
-			System.out.println("계좌정보가 없습니다.");
-		} else {
-			System.out.println("내 계좌정보 : " + acc.getAccountNo());
-			System.out.println("내 계좌 잔액 : " + acc.getMoney());
-			System.out.println("내 계좌 비번 : " + acc.getPassword());
-			System.out.println("내 통장" + acc.getTitle());
-			model.addAttribute("acc", acc);
-			model.addAttribute("id", userid);
-		}
-	
-		return "account/myAccount";
-	}*/
-	
-	/*
 	@Autowired
-	AccountServiceImpl accountService;
+	BoardVO board;
+	@Autowired
+	BoardServiceImpl service;
 
-	// 내 계좌페이지 이동
-	@RequestMapping(value = "/myAccount/{userid}", method = RequestMethod.GET)
-	public String myAccount(@PathVariable("userid") String userid, Model model) {
+	// 로그인 후 게시판 보기
+	@RequestMapping(value = "/boardList", method = RequestMethod.GET)
+	public String BoardList(Model model) {
 
-		AccountVO acc = new AccountVO();
-		acc = accountService.getAccount(userid);
-		if (acc == null) {
-			System.out.println("계좌정보가 없습니다.");
+		BoardVO[] boardList = service.selectBoard();
+		if (boardList == null) {
+			System.out.println("게시판글이 없습니다.");
 		} else {
-			System.out.println("내 계좌정보 : " + acc.getAccountNo());
-			System.out.println("내 계좌 잔액 : " + acc.getMoney());
-			System.out.println("내 계좌 비번 : " + acc.getPassword());
-			System.out.println("내 통장" + acc.getTitle());
-			model.addAttribute("acc", acc);
-			model.addAttribute("id", userid);
+			System.out.println("여기들어왔니~?");
+
+			model.addAttribute("boardList", boardList);
 		}
-	
-		return "account/myAccount";
+
+		return "board/boardList";
 	}
 
-	// 계좌 개설하기
-	@RequestMapping(value = "/openAccount", method = RequestMethod.GET)
-	public String openAccount(Model model, HttpSession session) {
-
-		return "account/myAccount";
+	// 게시판 글 작성 폼
+	@RequestMapping(value = "/boardInsertForm", method = RequestMethod.GET)
+	public String BoardInsertForm(Model model) {
+		return "board/boardInsertForm";
 	}
 
-	// 입금하기
-	@RequestMapping(value = "/deposit", method = RequestMethod.GET)
-	public String deposit(Model model, HttpSession session) {
+	/*// 게시판 글 작성
+	@RequestMapping(value = "/boardInsert", method = RequestMethod.GET)
+	public String BoardInsert(@RequestParam("title") String title, @RequestParam("context") String context,
+			HttpSession session, Model model) {
 
-		return "account/myAccount";
+		MemberVO temp = new MemberVO();
+		temp = (MemberVO) session.getAttribute("member");
+		
+		board.setId(temp.getId());
+		board.setTitle(title);
+		board.setContext(context);
+
+		if (board == null) {
+			System.out.println("내용이 없습니다.");
+		} else {
+			System.out.println("여기들어왔니~?");
+
+			model.addAttribute("boardList", boardList);
+		}
+
+		return "board/boardList";
 	}
-
-	// 출금하기
-	@RequestMapping(value = "/withdraw", method = RequestMethod.GET)
-	public String withdraw(Model model, HttpSession session) {
-
-		return "account/myAccount";
-	}
-
-	// 계좌해지
-	@RequestMapping(value = "/removeAccount", method = RequestMethod.GET)
-	public String removeAccount(Model model, HttpSession session) {
-
-		return "account/myAccount";
-	}*/
+*/
+	/*
+	 * @Autowired AccountServiceImpl accountService;
+	 * 
+	 * // 내 계좌페이지 이동
+	 * 
+	 * @RequestMapping(value = "/myAccount/{userid}", method =
+	 * RequestMethod.GET) public String myAccount(@PathVariable("userid") String
+	 * userid, Model model) {
+	 * 
+	 * AccountVO acc = new AccountVO(); acc = accountService.getAccount(userid);
+	 * if (acc == null) { System.out.println("계좌정보가 없습니다."); } else {
+	 * System.out.println("내 계좌정보 : " + acc.getAccountNo()); System.out.println(
+	 * "내 계좌 잔액 : " + acc.getMoney()); System.out.println("내 계좌 비번 : " +
+	 * acc.getPassword()); System.out.println("내 통장" + acc.getTitle());
+	 * model.addAttribute("acc", acc); model.addAttribute("id", userid); }
+	 * 
+	 * return "account/myAccount"; }
+	 * 
+	 * // 계좌 개설하기
+	 * 
+	 * @RequestMapping(value = "/openAccount", method = RequestMethod.GET)
+	 * public String openAccount(Model model, HttpSession session) {
+	 * 
+	 * return "account/myAccount"; }
+	 * 
+	 * // 입금하기
+	 * 
+	 * @RequestMapping(value = "/deposit", method = RequestMethod.GET) public
+	 * String deposit(Model model, HttpSession session) {
+	 * 
+	 * return "account/myAccount"; }
+	 * 
+	 * // 출금하기
+	 * 
+	 * @RequestMapping(value = "/withdraw", method = RequestMethod.GET) public
+	 * String withdraw(Model model, HttpSession session) {
+	 * 
+	 * return "account/myAccount"; }
+	 * 
+	 * // 계좌해지
+	 * 
+	 * @RequestMapping(value = "/removeAccount", method = RequestMethod.GET)
+	 * public String removeAccount(Model model, HttpSession session) {
+	 * 
+	 * return "account/myAccount"; }
+	 */
 }
--- SimpleProject/src/main/java/com/bank/web/serviceImpl/BoardServiceImpl.java
+++ SimpleProject/src/main/java/com/bank/web/serviceImpl/BoardServiceImpl.java
@@ -1,60 +1,29 @@
 package com.bank.web.serviceImpl;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bank.web.domain.BoardVO;
+import com.bank.web.mapper.BoardMapper;
 import com.bank.web.service.BoardService;
 
 @Service
 public class BoardServiceImpl implements BoardService {
 
-	@Override
-	public BoardVO[] selectBoard() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int insertBoard(String id, String title, String context) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	/*AccountVO account = new AccountVO();
-	
 	@Autowired
 	private SqlSession sqlSession;
 	
 	@Override
-	public String deposit(int money) {
-		// 2. 입금
-		account.setMoney(account.getMoney() + money);
-		return "잔액 : " + account.getMoney();
+	public BoardVO[] selectBoard() {
+		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+		return mapper.selectBoard();
 	}
-	
+
 	@Override
-	public String withdraw(int money) {
-		// 3. 출금  ALT + SHIFT + M : 메소드로 추출하는 단축키
-		return (account.getMoney() >= money) ? this.saveMoney(money) : "잔액 부족";
+	public int insertBoard(String id, String title, String context) {
+		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+		return mapper.insertBoard(id, title, context);
 	}
-	
-	private String saveMoney(int money) {
-		String result;
-		account.setMoney(account.getMoney() - money);
-		result = "잔액 : " + account.getMoney();
-		return result;
-	}
-	
-	@Override
-	public String search() {
-		// 4. 잔액조회
-		return "잔액 : " + account.getMoney();
-	}
-	
-	@Override
-	public AccountVO getAccount(String userid) {
-		AccountVO temp = new AccountVO();
-		AccountMapper mapper = sqlSession.getMapper(AccountMapper.class);
-		temp = mapper.selectAccount(userid);
-		return temp;
-	}*/
+
 }
